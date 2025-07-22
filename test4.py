0d9466c95c3f51b5ae9b89d5a2b660adcb6f4167
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2025-07-22 18:10:50 +0100

    commit

--- test4.py
+++ test4.py
@@ -54,10 +54,10 @@ while True:
         print('added at', row, col)
 
         # add a particle to that position...
-        np.append(pos, np.array([row, col]))
+        np.append(pos, np.array([row, col]), axis=0)
         
         # ... with zero velocity
-        np.append(vel, np.array([0, 0]))
+        np.append(vel, np.array([0, 0]), axis=0)
 
         print(pos)
 
